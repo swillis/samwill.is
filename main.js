785670befe0527d3c25b4570e3b92746031818c8
Sam Willis <samueljohnwillis@gmail.com>
2016-03-06 17:14:36 +0000

    Add timely greetings

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-  $('.content').addClass('reveal');
-})
+var d = new Date();
+var time = d.getHours();
+
+var setGreeting = function() {
+  if (time < 12) {
+    $('.greeting').text('Good morning!');
+  }
+  else if (time >= 12 && time < 18) {
+    $('.greeting').text('Good afternoon!');
+  }
+  else if (time >= 18) {
+    $('.greeting').text('Good evening!');
+  }
+}
+
+var revealContent = function() {
+  $(document).ready(function() {
+    $('.content').addClass('reveal');
+  })
+}
+
+revealContent();
+setGreeting();
